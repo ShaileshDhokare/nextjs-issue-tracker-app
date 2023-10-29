18c419224d854a39877f58c4e91c862cd64cd012
Shailesh Dhokare <shailesh19sd@gmail.com>
2023-10-29 14:50:50 +0530

    client side validation and refactor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
@@ -8,15 +8,23 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { BiError } from 'react-icons/bi';
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssue = () => {
   const [error, setError] = useState('');
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   return (
     <div className='max-w-xl'>
       {error && (
@@ -41,6 +49,11 @@ const NewIssue = () => {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name='description'
           control={control}
@@ -48,6 +61,11 @@ const NewIssue = () => {
             <SimpleMDE placeholder='Description' {...field} />
           )}
         />
+        {errors.description && (
+          <Text color='red' as='p'>
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
