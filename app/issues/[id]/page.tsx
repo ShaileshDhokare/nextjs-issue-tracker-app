59ec7517b070331350bc68f77dbd92e800ad1eac
Shailesh Dhokare <shailesh19sd@gmail.com>
2023-10-29 21:28:44 +0530

    styled issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,14 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
+import Markdown from 'react-markdown';
 
 interface Props {
   params: { id: string };
 }
 
 const page = async ({ params }: Props) => {
-  if (typeof params.id !== 'number') notFound();
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -15,10 +17,14 @@ const page = async ({ params }: Props) => {
   }
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className='space-x-3' my='2'>
+        <IssueStatusBadge status={issue.status}></IssueStatusBadge>
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   );
 };
