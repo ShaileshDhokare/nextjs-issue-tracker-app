13c9063de081b87c9e305d73a54adee71471ccca
Shailesh Dhokare <shailesh19sd@gmail.com>
2023-11-03 23:59:46 +0530

    login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,9 +4,13 @@ import classNames from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
+
   const links = [
     {
       label: 'Dashboard',
@@ -38,6 +42,14 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Log out</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
