cc9dd67f82f17e57c0af48558cdf22d87e7e0823
Shailesh Dhokare <shailesh19sd@gmail.com>
2023-11-25 15:35:25 +0530

    Build the pagination component layout

---
+++ app/issues/_components/Pagination.tsx
@@ -0,0 +1,59 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from '@radix-ui/react-icons';
+import { Button, Flex, Text } from '@radix-ui/themes';
+
+interface Props {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
+  const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount <= 1) return null;
+  return (
+    <Flex>
+      <Text>
+        Page {currentPage} of {pageCount}
+      </Text>
+      <Button
+        color='gray'
+        variant='soft'
+        disabled={currentPage === 1}
+        // onClick={() => changePage(1)}
+      >
+        <DoubleArrowLeftIcon />
+      </Button>
+      <Button
+        color='gray'
+        variant='soft'
+        disabled={currentPage === 1}
+        // onClick={() => changePage(currentPage - 1)}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Button
+        color='gray'
+        variant='soft'
+        disabled={currentPage === pageCount}
+        // onClick={() => changePage(currentPage + 1)}
+      >
+        <ChevronRightIcon />
+      </Button>
+      <Button
+        color='gray'
+        variant='soft'
+        disabled={currentPage === pageCount}
+        // onClick={() => changePage(pageCount)}
+      >
+        <DoubleArrowRightIcon />
+      </Button>
+    </Flex>
+  );
+};
+
+export default Pagination;
