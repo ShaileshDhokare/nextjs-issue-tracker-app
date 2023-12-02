cff68ac9b6bdc0465df6e1f8b8be14a92318ba64
Shailesh Dhokare <shailesh19sd@gmail.com>
2023-12-02 12:50:46 +0530

    implement pagination

--- app/issues/_components/Pagination.tsx
+++ app/issues/_components/Pagination.tsx
@@ -5,6 +5,7 @@ import {
   DoubleArrowRightIcon,
 } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   itemCount: number;
@@ -13,8 +14,18 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(itemCount / pageSize);
+
   if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  };
+
   return (
     <Flex>
       <Text>
@@ -24,7 +35,7 @@ const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
         color='gray'
         variant='soft'
         disabled={currentPage === 1}
-        // onClick={() => changePage(1)}
+        onClick={() => changePage(1)}
       >
         <DoubleArrowLeftIcon />
       </Button>
@@ -32,7 +43,7 @@ const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
         color='gray'
         variant='soft'
         disabled={currentPage === 1}
-        // onClick={() => changePage(currentPage - 1)}
+        onClick={() => changePage(currentPage - 1)}
       >
         <ChevronLeftIcon />
       </Button>
@@ -40,7 +51,7 @@ const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
         color='gray'
         variant='soft'
         disabled={currentPage === pageCount}
-        // onClick={() => changePage(currentPage + 1)}
+        onClick={() => changePage(currentPage + 1)}
       >
         <ChevronRightIcon />
       </Button>
@@ -48,7 +59,7 @@ const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
         color='gray'
         variant='soft'
         disabled={currentPage === pageCount}
-        // onClick={() => changePage(pageCount)}
+        onClick={() => changePage(pageCount)}
       >
         <DoubleArrowRightIcon />
       </Button>
